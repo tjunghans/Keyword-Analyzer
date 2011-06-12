3e4ad87d6eec33878ba66016386ebe4832fb139b
Daniel Kummer <0r1g4m14dd1c7@gmail.com>
2011-06-12 23:20:58 +0200

    add form (non interactive) add logging setup

---
+++ src/main/java/za/co/junghans/hszt/semin/ir/KeywordAnalyzer.java
@@ -0,0 +1,26 @@
+package za.co.junghans.hszt.semin.ir;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class KeywordAnalyzer
+{
+    public static void main( String[] args )
+    {
+        showUI();
+    }
+
+    private static void showUI() {
+        JFrame frame = new JFrame("Keyword Analyzer ");
+        frame.setPreferredSize(new Dimension(600, 500));
+        frame.setLocationRelativeTo(null);
+        frame.setContentPane(new AnalyzerGui().getForm());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ src/test/java/za/co/junghans/hszt/semin/ir/KeywordAnalyzerTest.java
@@ -0,0 +1,38 @@
+package za.co.junghans.hszt.semin.ir;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple KeywordAnalyzer.
+ */
+public class KeywordAnalyzerTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public KeywordAnalyzerTest(String testName)
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( KeywordAnalyzerTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
